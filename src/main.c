fe71e55567b94d619c451208e1a53973a8b7f89a
pwam <ryandclements@gmail.com>
2025-02-06 09:38:14 -0800

    Laying out skeletal structure of main.c, mostly placeholder.

--- src/main.c
+++ src/main.c
@@ -3,81 +3,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_FONTS 8
+// ------------------------------------------------------------
+// Definitions and Structures
+
+// TODO HERE
+// ------------------------------------------------------------
+
+
+// ------------------------------------------------------------
+// Program Entry Point
+// ------------------------------------------------------------
 
 int main(void)
 {
+    // Initialization
+    // ------------------------------------------------------------
 
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "game testing");
+    InitWindow(screenWidth, screenHeight, "Kill the King");
 
-    Font fonts[MAX_FONTS] = { 0 };
-
-    fonts[0] = LoadFont("resources/fonts/alagard.png");
-    fonts[1] = LoadFont("resources/fonts/pixelplay.png");
-    fonts[2] = LoadFont("resources/fonts/mecha.png");
-    fonts[3] = LoadFont("resources/fonts/setback.png");
-    fonts[4] = LoadFont("resources/fonts/romulus.png");
-    fonts[5] = LoadFont("resources/fonts/pixantiqua.png");
-    fonts[6] = LoadFont("resources/fonts/alpha_beta.png");
-    fonts[7] = LoadFont("resources/fonts/jupiter_crash.png");
-
-    const char *messages[MAX_FONTS] = { "ALAGARD FONT designed by Hewett Tsoi",
-                                "PIXELPLAY FONT designed by Aleksander Shevchuk",
-                                "MECHA FONT designed by Captain Falcon",
-                                "SETBACK FONT designed by Brian Kent (AEnigma)",
-                                "ROMULUS FONT designed by Hewett Tsoi",
-                                "PIXANTIQUA FONT designed by Gerhard Grossmann",
-                                "ALPHA_BETA FONT designed by Brian Kent (AEnigma)",
-                                "JUPITER_CRASH FONT designed by Brian Kent (AEnigma)" };
-
-    const int spacings[MAX_FONTS] = { 10, 4, 8, 4, 3, 4, 4, 1 };
- 
-    Vector2 positions[MAX_FONTS] = { 0 };
-
-    for (int i = 0; i < MAX_FONTS; i++)
-    {
-        positions[i].x = screenWidth/2.0f - MeasureTextEx(fonts[i], messages[i], fonts[i].baseSize*2.0f, (float)spacings[i]).x/2.0f;
-        positions[i].y = 60.0f + fonts[i].baseSize + 45.0f*i;
-    }
-
-    // Small Y position corrections
-    positions[3].y += 8;
-    positions[4].y += 2;
-    positions[7].y -= 8;
-
-    // Color colors[MAX_FONTS] = { MAROON, ORANGE, DARKGREEN, DARKBLUE, DARKPURPLE, LIME, GOLD, RED };
-    Color colors[MAX_FONTS] = { RAYWHITE, RAYWHITE, RAYWHITE, RAYWHITE, RAYWHITE, RAYWHITE, RAYWHITE, RAYWHITE };
-    
+    SetExitKey(KEY_NULL);
 
     SetTargetFPS(60);
 
+    int framesCounter = 0;
+
+    Vector2 testPosition = { 200.0f, 200.0f };
+
+    Vector2 mousePosition;
+
+    // Font Settings
+    Font mainFont = LoadFont("resources/fonts/pixantiqua.png");
+
+    // ------------------------------------------------------------
+
+    // Core Game Loop
     while (!WindowShouldClose())
     {
+
+        // Update
+        // ------------------------------------------------------------
+        framesCounter++;
+
+
+        // Draw
+        // ------------------------------------------------------------
+
         BeginDrawing();
 
             ClearBackground(BLACK);
 
-            DrawText("free fonts included with raylib", 250, 20, 20, DARKGRAY);
-            DrawLine(220, 50, 590, 50, DARKGRAY);
-
-            for (int i = 0; i < MAX_FONTS; i++)
-            {
-                DrawTextEx(fonts[i], messages[i], positions[i], fonts[i].baseSize*2.0f, (float)spacings[i], colors[i]);
-            }
+            DrawTextEx(mainFont, "Testing", testPosition, (float)20, (float)4, WHITE);
             
         EndDrawing();
+
+        // ------------------------------------------------------------
     }
 
-    for (int i = 0; i < MAX_FONTS; i++)
-    {
-        UnloadFont(fonts[i]);
-    }
-    
+    // De-Initialization
+    // ------------------------------------------------------------
+
+    UnloadFont(mainFont);
 
     CloseWindow();
+
+    // ------------------------------------------------------------
 
     return 0;
 
