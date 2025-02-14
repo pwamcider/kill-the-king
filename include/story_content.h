2c0e94361b968cf25bf5543439768763c8aafc3d
pwam <ryandclements@gmail.com>
2025-02-13 21:33:11 -0800

    Moved Page and Option struct definitions into file.

--- include/story_content.h
+++ include/story_content.h
@@ -1,1 +1,22 @@
 #pragma once
+
+// Structure Definitions
+// ------------------------------------------------------------
+
+typedef struct Option
+{
+    char prompt[256];
+    struct Event* to_event;
+} Option;
+
+typedef struct Page
+{
+    char text[2048];
+    struct Option options[4];
+    bool checkpoint;
+    bool game_over;
+    bool raised_alarm;
+    bool* page_visited;
+} Page;
+
+// ------------------------------------------------------------
