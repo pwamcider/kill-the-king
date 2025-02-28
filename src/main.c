2e3ff8b615b38961d254be799dacdc0fd6094d2a
pwam <ryandclements@gmail.com>
2025-02-28 10:31:37 -0800

    Simplified main; removed button definitions as they now live in seperate file; adjusted calls to BUTTON struct.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "buttons.h"
 #include "functions.h"
 #include "initialize.h"
 #include "raylib.h"
@@ -15,20 +16,6 @@ int main(void)
     InitGame();
 
     ResourceLoad();
-
-    // Button Setup
-    enum ButtonState {
-        BUTTON_IDLE = 0,
-        BUTTON_HOVER = 1,
-        BUTTON_PRESSED = 2,
-    };
-
-    // TODO - clean all this stuff up!
-    enum ButtonState buttonState;
-    int buttonNumFrames = 3;
-    float buttonFrameHeight = (float)buttonSprite.height/buttonNumFrames;
-    int buttonAlignmentX = 850;
-    Vector2 buttonSpriteOrigin = { 0, 0 };
 
     // Line Setup
     float lineThickness = 1;
@@ -63,35 +50,34 @@ int main(void)
         // ------------------------------------------------------------
         framesCounter++;
         mousePosition = GetMousePosition();
+        BUTTON.frameHeight = GetButtonSpriteHeight()/BUTTON.numFrames;
 
         int numOptions = CountOptions(currentPage);
-        int buttonOffsetY = 1;
+        BUTTON.offsetY = 1;
         Rectangle buttonSource[numOptions];
         Rectangle buttonDest[numOptions];
-        // TODO - may need to minus 1 for the above array, since numOptions returns the total count
-        // but array is zero indexed
 
         for (int i = 0; i < numOptions; i++)
         {
             buttonSource[i].x = 0;
             buttonSource[i].y = 0;
             buttonSource[i].width = (float)buttonSprite.width;
-            buttonSource[i].height = buttonFrameHeight;
+            buttonSource[i].height = BUTTON.frameHeight;
             
-            buttonDest[i].x = buttonAlignmentX;
-            buttonDest[i].y = buttonOffsetY * (float)buttonSprite.height/buttonNumFrames;
+            buttonDest[i].x = BUTTON.alignmentX;
+            buttonDest[i].y = BUTTON.offsetY * (float)buttonSprite.height/BUTTON.numFrames;
             buttonDest[i].width = (float)buttonSprite.width;
-            buttonDest[i].height = buttonFrameHeight;
+            buttonDest[i].height = BUTTON.frameHeight;
 
             const bool isMouseOver = CheckCollisionPointRec(mousePosition, buttonDest[i]);
 
             if (isMouseOver)
             {
-                buttonState = IsMouseButtonDown(MOUSE_BUTTON_LEFT) ? BUTTON_PRESSED : BUTTON_HOVER;
+                BUTTON.state = IsMouseButtonDown(MOUSE_BUTTON_LEFT) ? 1 : 2;
             }
             else
             {
-                buttonState = BUTTON_IDLE;
+                BUTTON.state = 0;
             }
 
             if (isMouseOver && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
@@ -101,9 +87,9 @@ int main(void)
                 currentPage = currentPage->options[i].toPage;
             }
             
-            buttonSource[i].y = buttonState*buttonFrameHeight;
+            buttonSource[i].y = BUTTON.state*BUTTON.frameHeight;
 
-            buttonOffsetY++;
+            BUTTON.offsetY++;
         }
 
         // Draw
@@ -117,7 +103,7 @@ int main(void)
 
             for (int i = 0; i < numOptions; i++)
             {
-                DrawTexturePro(buttonSprite, buttonSource[i], buttonDest[i], buttonSpriteOrigin, 0, WHITE);
+                DrawTexturePro(buttonSprite, buttonSource[i], buttonDest[i], BUTTON.spriteOrigin, 0, WHITE);
             }
         
             DrawLineEx(lineStart, lineStop, lineThickness, lineColor);
