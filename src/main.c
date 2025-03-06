bdec307f064864a96a95248c1141abac35c863ba
pwam <ryandclements@gmail.com>
2025-03-05 21:36:36 -0800

    Moved all update and draw code to update.c/h files; removed redundant #includes now that they live in update files.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,7 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "buttons.h"
-#include "functions.h"
 #include "initialize.h"
 #include "raylib.h"
 #include "resources.h"
-#include "story_content.h"
-#include "text.h"
-#include "ui.h"
-
-// ------------------------------------------------------------
+#include "update.h"
 
 int main(void)
 {
@@ -19,98 +9,14 @@ int main(void)
 
     ResourceLoad();
 
-    // Startup Variables
-    int framesCounter = 0;
-    Vector2 mousePosition;
-    Page *currentPage = &TEST1;
-
-    // ------------------------------------------------------------
-
-    // Core Game Loop
     while (!WindowShouldClose())
     {
-
-        // Update
-        // ------------------------------------------------------------
-        framesCounter++;
-        mousePosition = GetMousePosition();
-        BUTTON.frameHeight = GetButtonSpriteHeight()/BUTTON.numFrames;
-
-        int numOptions = CountOptions(currentPage);
-        BUTTON.offsetY = 1;
-        Rectangle buttonSource[numOptions];
-        Rectangle buttonDest[numOptions];
-
-        for (int i = 0; i < numOptions; i++)
-        {
-            buttonSource[i].x = 0;
-            buttonSource[i].y = 0;
-            buttonSource[i].width = (float)buttonSprite.width;
-            buttonSource[i].height = BUTTON.frameHeight;
-            
-            buttonDest[i].x = BUTTON.alignmentX;
-            buttonDest[i].y = BUTTON.offsetY * (float)buttonSprite.height/BUTTON.numFrames;
-            buttonDest[i].width = (float)buttonSprite.width;
-            buttonDest[i].height = BUTTON.frameHeight;
-
-            const bool isMouseOver = CheckCollisionPointRec(mousePosition, buttonDest[i]);
-
-            if (isMouseOver)
-            {
-                BUTTON.state = IsMouseButtonDown(MOUSE_BUTTON_LEFT) ? 1 : 2;
-            }
-            else
-            {
-                BUTTON.state = 0;
-            }
-
-            if (isMouseOver && IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-            {
-                PlaySound(buttonSound);
-                framesCounter = 0;
-                currentPage = currentPage->options[i].toPage;
-            }
-            
-            buttonSource[i].y = BUTTON.state*BUTTON.frameHeight;
-
-            BUTTON.offsetY++;
-        }
-
-        // Draw
-        // ------------------------------------------------------------
-
-        BeginDrawing();
-
-            ClearBackground(BLACK);
-            
-            SetTextLineSpacing(TEXT.vertSpacing);
-
-            for (int i = 0; i < numOptions; i++)
-            {
-                DrawTexturePro(buttonSprite, buttonSource[i], buttonDest[i], BUTTON.spriteOrigin, 0, WHITE);
-            }
-            DrawLineEx(GetLineStart(), GetLineStop(), LINE.thickness, LINE.color);
-
-            DrawTextPro(
-                textFont,
-                TextSubtext(currentPage->text, 0, framesCounter/TEXT.printSpeed),
-                TEXT.position, TEXT.origin, TEXT.rotation, TEXT.size, TEXT.spacing, TEXT.color
-            );
-            
-        EndDrawing();
-        
-        // ------------------------------------------------------------
+        UpdateGame();
     }
-
-    // De-Initialization
-    // ------------------------------------------------------------
 
     ResourceUnload();
 
     ShutdownGame();
 
-    // ------------------------------------------------------------
-
     return 0;
-
 }
