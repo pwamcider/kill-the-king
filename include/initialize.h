32cadd74c99308a28dc162410b1612a7831d3fc2
pwam <ryandclements@gmail.com>
2025-03-10 07:57:12 -0700

    Moved screen sizes into UI file; removed redundant variable definitions and functions from initialize.

--- include/initialize.h
+++ include/initialize.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "raylib.h"
+#include "ui.h"
 
 // Prototypes
 // ------------------------------------------------------------
 
-int FindScreenHeight(void);
-int FindScreenWidth(void);
 void InitGame(void);
 void ResourceLoad(void);
 void ShutdownGame(void);
--- src/initialize.c
+++ src/initialize.c
@@ -1,24 +1,10 @@
 #include "initialize.h"
 
-// Screen
-// ------------------------------------------------------------
-
-int screenWidth = 1280;
-int screenHeight = 720;
-
 // Functions
 // ------------------------------------------------------------
 
-int FindScreenWidth(void) {
-    return screenWidth;
-}
-
-int FindScreenHeight(void) {
-    return screenHeight;
-}
-
  void InitGame(void){
-    InitWindow(screenWidth, screenHeight, "Kill the King");
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, "Kill the King");
     InitAudioDevice();
 
     SetExitKey(KEY_NULL);
