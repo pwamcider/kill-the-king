39a33e2fe1f54c56e852efcd11731bd0878241a9
pwam <ryandclements@gmail.com>
2025-03-10 07:56:30 -0700

    Deleting seperate button file; moved contents into UI file for ease of access.

--- src/buttons.c
+++
@@ -1,9 +0,0 @@
-#include "buttons.h"
-
-Button BUTTON = {
-    .frameHeight = 0.0,
-    .numFrames = 3,
-    .state = 0, // 0 = Idle, 1 = Hover, 2 = Pressed
-    .color = WHITE,
-    .spriteOrigin = { 0.0f, 0.0f },
-};
