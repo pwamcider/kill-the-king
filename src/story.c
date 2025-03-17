18e6a7629f411be84155149e0db1b71f56406aec
pwam <ryandclements@gmail.com>
2025-03-17 09:45:03 -0700

    Added CheckForConsequence function; added test content.

--- src/story.c
+++ src/story.c
@@ -30,6 +30,18 @@ int CountOptions(Page* page)
     return numOptions;
 }
 
+void CheckForConsequence(Option* option)
+{
+    if (option->toConsequence != NULL)
+    {
+        Option* target = option->toConsequence->target;
+        Page* redirect = option->toConsequence->redirect;
+
+        target->toPage = redirect;
+    }
+    return;
+}
+
 void PlayPage(Page* page)
 {
     if (page->checkpoint)
@@ -55,14 +67,37 @@ void PlayPage(Page* page)
 Page TEST1 = {
     .text = "You're standing in the first room.",
     .options = {
-        { .prompt = "Go to the second room.", .toPage = &TEST2 },
+        { .prompt = "Go to the second room.", .toPage = &TEST2, .toConsequence = NULL },
+        { .prompt = "Dance a bit.", .toPage = &DANCE }
+    },
+};
+
+Page DANCE = {
+    .text = "You dance around a bit, and feel real good.",
+    .options = {
+        { .prompt = "Continue.", .toPage = &TEST1, .toConsequence = NULL }
+    },
+};
+
+Page DANCE_FAIL = {
+    .text = "You can't dance right now. Because of the leg, remember?",
+    .options = {
+        { .prompt = "Continue.", .toPage = &TEST1, .toConsequence = NULL }
     },
 };
 
 Page TEST2 = {
     .text = "You're standing in the second room.",
     .options = {
-        { .prompt = "Go to the first room.", .toPage = &TEST1 },
-        { .prompt = "Run to the first room.", .toPage = &TEST1 },
+        { .prompt = "Go to the first room.", .toPage = &TEST1, .toConsequence = NULL },
+        { .prompt = "Break leg on way to first room.", .toPage = &TEST1, .toConsequence = &BROKE_LEG },
     },
+};
+
+// Consequences
+// ------------------------------------------------------------
+
+Consequence BROKE_LEG = {
+    .target = &TEST1.options[1],
+    .redirect = &DANCE_FAIL,
 };
