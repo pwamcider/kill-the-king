3e6c1497097181374dcdacb2389d95fd9733f954
pwam <ryandclements@gmail.com>
2025-03-17 09:45:14 -0700

    Added consequence struct.

--- include/story.h
+++ include/story.h
@@ -6,11 +6,20 @@
 // Structures
 // ------------------------------------------------------------
 
+/* TODO - an "updater" system. Uses a mix of structs that contain what the updates will be,
+what flags they are triggered by and what changes they make to pages. Then, functions that run during game
+loop that will trigger when a flag is detected. It then dynamically updates page content, either changing the text,
+changing the prompts, or changing the pointers to redirect to a new node.
+
+This system would require that Page structs have a pointer to specific update nodes. 
+*/
+
 struct Page;
 
 typedef struct Option
 {
     char prompt[256];
+    struct Consequence* toConsequence;
     struct Page* toPage;
 } Option;
 
@@ -21,20 +30,31 @@ typedef struct Page
     bool checkpoint;
     bool gameOver;
     bool raisedAlarm;
-    bool* pageVisited;
 } Page;
 
-
-// Pages
-// ------------------------------------------------------------
-
-extern Page TEST1;
-extern Page TEST2;
-
+typedef struct Consequence
+{
+    Option* target;
+    Page* redirect;
+} Consequence;
 
 // Prototypes
 // ------------------------------------------------------------
 
 bool IsOptionValid(Option* option);
 int CountOptions(Page* page);
+void CheckForConsequence(Option* option);
 void PlayPage(Page* page);
+
+// Pages
+// ------------------------------------------------------------
+
+extern Page TEST1;
+extern Page TEST2;
+extern Page DANCE;
+extern Page DANCE_FAIL;
+
+// Consequences
+// ------------------------------------------------------------
+
+extern Consequence BROKE_LEG;
