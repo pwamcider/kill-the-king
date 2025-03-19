e6c380660f8c14543579da517c344bcad5f55d05
pwam <ryandclements@gmail.com>
2025-03-18 20:14:45 -0700

    Expanded consequence functionality to be part of Ripple struct.

--- src/story.c
+++ src/story.c
@@ -24,20 +24,48 @@ int CountOptions(Page* page)
         {
             break;
         }
-        numOptions++;
+        numOptions += 1;
         
     }
     return numOptions;
 }
 
-void CheckForConsequence(Option* option)
+bool IsConsequenceValid(Consequence* consequence)
 {
-    if (option->toConsequence != NULL)
-    {
-        Option* target = option->toConsequence->target;
-        Page* redirect = option->toConsequence->redirect;
+    return consequence && consequence->target && consequence->redirect; 
+}
 
-        target->toPage = redirect;
+int CountConsequences(Ripple* ripple)
+{
+    int numConsequences = 0;
+
+    for (int i = 0; i < sizeof(ripple->consequences); i++)
+    {
+        if (!IsConsequenceValid(&ripple->consequences[i]))
+        {
+            break;
+        }
+        numConsequences += 1;
+    }
+    return numConsequences;
+}
+
+void CheckForRipple(Option* option)
+{
+
+    if (option->toRipple != NULL)
+    {
+        Ripple *ripple = option->toRipple;
+
+        int numConsequences = CountConsequences(ripple);
+
+        for (int i = 0; i < numConsequences; i++)
+        {
+            Option* target = ripple->consequences[i].target;
+            Page* redirect = ripple->consequences[i].redirect;
+
+            target->toPage = redirect;
+        }
     }
     return;
 }
@@ -67,37 +95,46 @@ void PlayPage(Page* page)
 Page TEST1 = {
     .text = "You're standing in the first room.",
     .options = {
-        { .prompt = "Go to the second room.", .toPage = &TEST2, .toConsequence = NULL },
+        { .prompt = "Go to the second room.", .toPage = &TEST2 },
         { .prompt = "Dance a bit.", .toPage = &DANCE }
-    },
+    }
 };
 
 Page DANCE = {
     .text = "You dance around a bit, and feel real good.",
     .options = {
-        { .prompt = "Continue.", .toPage = &TEST1, .toConsequence = NULL }
-    },
+        { .prompt = "Continue.", .toPage = &TEST1 }
+    }
 };
 
 Page DANCE_FAIL = {
     .text = "You can't dance right now. Because of the leg, remember?",
     .options = {
-        { .prompt = "Continue.", .toPage = &TEST1, .toConsequence = NULL }
-    },
+        { .prompt = "Continue.", .toPage = &TEST1 }
+    }
 };
 
 Page TEST2 = {
     .text = "You're standing in the second room.",
     .options = {
-        { .prompt = "Go to the first room.", .toPage = &TEST1, .toConsequence = NULL },
-        { .prompt = "Break leg on way to first room.", .toPage = &TEST1, .toConsequence = &BROKE_LEG },
-    },
+        { .prompt = "Go to the first room.", .toPage = &TEST1, .toRipple = NULL },
+        { .prompt = "Break leg on way to first room.", .toPage = &TEST1, .toRipple = &BROKE_LEG },
+    }
+};
+
+Page TEST2_BROKEN = {
+    .text = "You're standing wobbly in the second room on one leg.",
+    .options = {
+        { .prompt = "Hobble to the first room.", .toPage = &TEST1 },
+    }
 };
 
 // Consequences
 // ------------------------------------------------------------
 
-Consequence BROKE_LEG = {
-    .target = &TEST1.options[1],
-    .redirect = &DANCE_FAIL,
+Ripple BROKE_LEG = {
+    .consequences = {
+        { .target = &TEST1.options[0], .redirect = &TEST2_BROKEN },
+        { .target = &TEST1.options[1], .redirect = &DANCE_FAIL }
+    }
 };
