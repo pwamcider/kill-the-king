825d3f56dbbd42d40510f5ef03f7b4da45038499
pwam <ryandclements@gmail.com>
2025-03-21 07:50:32 -0700

    Moved header files into corresponding .c file

--- include/loop.h
+++ include/loop.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "raylib.h"
-#include "resources.h"
-#include "setup.h"
-#include "story_engine.h"
-#include "ui.h"
+// TODO : move all headers over to .c files.
 
 // ------------------------------------------------------------
 
