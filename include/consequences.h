2345b98e1e2b0f2e8afffc2a0f17bd3b47474677
pwam <ryandclements@gmail.com>
2025-03-25 10:09:20 -0700

    Declared arrays for tracking Ripples being used, and which ones are active during play.

--- include/consequences.h
+++ include/consequences.h
@@ -2,7 +2,9 @@
 
 #include "pages.h"
 
-// Struct
+#define NUM_RIPPLES 1
+
+// Structs
 // ------------------------------------------------------------
 
 typedef struct Consequence
@@ -15,6 +17,17 @@ typedef struct Ripple
 {
     struct Consequence consequences[5];
 } Ripple;
+
+// Tracking
+// ------------------------------------------------------------
+
+extern Ripple *ALL_RIPPLES[NUM_RIPPLES];
+extern bool ACTIVE_RIPPLES[NUM_RIPPLES];
+
+// Prototypes
+// ------------------------------------------------------------
+
+void ResetRipples(void);
 
 // Consequences
 // ------------------------------------------------------------
