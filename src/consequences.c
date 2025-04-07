2a37b88d2e5f96e0cad8480cd965a824af9da718
pwam <ryandclements@gmail.com>
2025-04-07 09:26:15 -0700

    Adding CHECKPOINT_RIPPLES to reset function.

--- src/consequences.c
+++ src/consequences.c
@@ -4,11 +4,13 @@
 // ------------------------------------------------------------
 
 bool ACTIVE_RIPPLES[NUM_RIPPLES];
+bool CHECKPOINT_RIPPLES[NUM_RIPPLES];
 
-void ResetRipples(void) {
+void ResetAllRipples(void) {
     for (int i = 0; i < NUM_RIPPLES; i++)
     {
         ACTIVE_RIPPLES[i] = false;
+        CHECKPOINT_RIPPLES[i] = false;
     }
 }
 
