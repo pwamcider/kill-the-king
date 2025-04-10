aee7415052f42f3b3715faa4374d619bb0ac4a88
pwam <ryandclements@gmail.com>
2025-04-10 09:16:29 -0700

    Added UpdateGame(), adjusting all update logic and draw functions to be controlled by this switch.

--- src/update.c
+++ src/update.c
@@ -1,5 +1,29 @@
+#include "draw.h"
+#include "loop.h"
+#include "pages.h"
+#include "story_engine.h"
+#include "ui.h"
 #include "update.h"
 
-// Game State
+// Game State Manager
 // ------------------------------------------------------------
 
+void UpdateGame(void) {
+    switch (GAME_STATE)
+    {
+    case MAIN_MENU:
+        GameLoop();
+        DrawMain();
+        if (CURRENT_PAGE != &MENU0)
+        {
+            GAME_STATE = GAME_LOOP;
+        }
+        break;
+    case GAME_LOOP:
+        GameLoop();
+        DrawLoop();
+        break;
+    default:
+        break;
+    }
+}
