9d0d03433b869a820d956b90eb188c99fc953213
pwam <ryandclements@gmail.com>
2025-04-10 09:12:07 -0700

    New draw files; moved all draw functionality here,; refactored DrawLoop() function to move possibly repeated draws into their own bespoke functions.

---
+++ include/draw.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// ------------------------------------------------------------
+
+void DrawButtons(void);
+void DrawDivider(void);
+void DrawLoop(void);
+void DrawMain(void);
+void DrawPrompt(void);
---
+++ src/draw.c
@@ -0,0 +1,60 @@
+#include "draw.h"
+#include "raylib.h"
+#include "resources.h"
+#include "setup.h"
+#include "story_engine.h"
+#include "ui.h"
+
+void DrawButtons(void) {
+    for (int i = 0; i < LAYOUT.numOptions; i++)
+    {
+        DrawTexturePro(buttonSprite, LAYOUT.buttonSource[i], LAYOUT.buttonDest[i],
+                        BUTTON.spriteOrigin, BUTTON.rotation, WHITE);
+        DrawTextPro(
+            textFont,
+            CURRENT_PAGE->options[i].prompt,
+            CalculatePromptPos(LAYOUT.buttonDest[i].x, LAYOUT.buttonDest[i].y),
+            TEXT.origin, TEXT.rotation, TEXT.size, TEXT.spacing, TEXT.color
+        );
+    }
+}
+
+void DrawDivider(void) {
+    DrawLineEx(LINE.start, LINE.stop, LINE.thickness, LINE.color);
+}
+
+void DrawPrompt(void) {
+    DrawTextPro(
+        textFont,
+        TextSubtext(CURRENT_PAGE->text, TEXT.printStartPos, *FRAME_COUNTER_PTR/TEXT.printSpeed),
+                    TEXT.position, TEXT.origin, TEXT.rotation, TEXT.size, TEXT.spacing, TEXT.color
+    );
+}
+
+void DrawLoop(void) {
+    BeginDrawing();
+
+            ClearBackground(BLACK);
+            
+            SetTextLineSpacing(TEXT.vertSpacing);
+
+            DrawButtons();
+
+            DrawDivider();
+
+            DrawPrompt();
+
+    EndDrawing();
+}
+
+void DrawMain(void) {
+    BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        SetTextLineSpacing(TEXT.vertSpacing);
+
+        DrawButtons();
+
+    EndDrawing();
+}
